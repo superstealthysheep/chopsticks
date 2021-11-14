eb0d445b33cd6453d882027a03cec6323c0ab7c8
William Walker <24390822+superstealthysheep@users.noreply.github.com>
2021-11-13 19:16:43 -0800

    Added logger experiments

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import chopsticksGame
 import players
 import presets
+import logger
 
 def main():
-  rg = presets.RandomGame(2, 3, 10) #a game with 2 players, each with 3 hands with 10 fingers
-  rg.run_game()
+  rg = logger.DepthFirstGame(2, 2, 5) #a game with 2 players, each with 3 hands with 10 fingers
+  l = logger.Logger(rg)
+  
+  l.run_game()
+  # for g in l.gamestate_list:
+  #   g.print()
+  # for mv in l.history:
+  #   print(mv)
 
+  # g_node = logger.GamestateNode(rg)
+  # print(g_node.gamestate.active_player_index)
+
+  # rg = presets.RandomGame(2)
+  # print(rg.players[rg.active_player_index].list_all_moves(rg))
 
 main()
