63101b841ad32deb32a39ce059a2c7069b8d277d
William Walker <24390822+superstealthysheep@users.noreply.github.com>
2021-11-13 19:18:42 -0800

    Added Move class, functions for listing stuff

--- chopsticksGame.py
+++ chopsticksGame.py
@@ -1,8 +1,12 @@
+# import copy
+
 class Hand:
+  id_counter = 0;
   def __init__(self, owner, size=5, fingers=0):
-    self.fingers = fingers;
+    self.fingers = fingers
     self.size = size;
     self.owner = owner
+    self.id = len(owner.hands) #this id is owner-specific only (different owners each have their own hand number 0)
 
   def __str__(self):
     return str(self.fingers)
@@ -60,9 +64,11 @@ class Player:
         live_hands += 1
         finger_total += hand.fingers
 
-    return (live_hands == 1) and (finger_total % len(self.hands) == 0) #a lonely hand that can be evenly divided
-    
-    
+    lonely_hand = live_hands == 1
+    has_partners = len(self.hands) > 1
+    evenly_divisible = (finger_total % len(self.hands) == 0)
+
+    return lonely_hand and has_partners and evenly_divisible #a lonely hand (with dead partners) that can be evenly divided
   
   def split(self):
     tot = 0
@@ -87,14 +93,68 @@ class Player:
       if hand.fingers:
         self.alive = True
     return self.alive
+
+  def list_targets(self, gamestate): #assemble list of potential targets
+    targets = []
+    for player in gamestate.players:
+      if player != self and player.alive:
+        targets.append(player)
+      
+    return targets
+
+  def list_target_hands(self, target_player):
+    target_hands = []
+    for hand in target_player.hands:
+      if hand.fingers:
+        target_hands.append(hand)
+    
+    return target_hands
+
+  def list_attack_hands(self):
+    attack_hands = []
+    for hand in self.hands:
+      if hand.fingers:
+        attack_hands.append(hand)
+
+    return attack_hands
+
+  def list_all_moves(self, gamestate): #could be buggy if self not in gamestate.players
+    all_moves = []
+    if self.can_split():
+      all_moves.append(Move("split", self.hands[0])) #this self.hands[0] thing is just my lazy, janky way to let Move know what player is doing the splitting
+    else:
+      for attack_hand in self.list_attack_hands():
+        for target_player in self.list_targets(gamestate):
+          for target_hand in self.list_target_hands(target_player):
+            all_moves.append(Move("attack", attack_hand, target_player, target_hand))
+    return all_moves
     
   
 class Gamestate:
   def __init__(self):
     self.players = []
     self.active_player_index = 0
+    self.active_player = None
     self.parents = []
     self.children = []
+
+  # @classmethod
+  # def construct_from_data(cls, data):
+  #   new_instance = cls()
+  #   new_instance.players = data[0]
+  #   new_instance.active_player_index = data[1]
+  #   new_instance.parents = data[2]
+  #   new_instance.children = data[3] #this seems messy/wrong
+  #   return new_instance
+
+  # @classmethod
+  # def deep_copy(cls, original):
+  #   data = copy.deepcopy(original._data())
+  #   return cls.construct_from_data(data)
+
+  # def _data(self):
+  #   data = [self.players, self.active_player_index, self.parents, self.children]
+  #   return data
 
   def add(self, player):
     self.players.append(player)
@@ -107,7 +167,9 @@ class Gamestate:
         for i in range(self.players.len()):
           if not self.players[i].equals(state.players[i]):
             return False
-
+      if(self.active_player_index != state.active_player_index):
+        return False
+      else:
         return True #returns True only if all the players are the same
   
   def done(self): #returns True after only one (or fewer) people are left standing 
@@ -128,7 +190,7 @@ class Gamestate:
       else:
         return False
 
-  def print(self):
+  def print(self): #should eventually do this through a __str__(), I think
     print("Gamestate:")
     for i in range(len(self.players)):
       if i == self.active_player_index:
@@ -147,13 +209,32 @@ class Gamestate:
     while not self.done():
       active_player = self.players[self.active_player_index]
 
-      active_player.strategize(self)
-      target_player = active_player.target_player
-      target_hand = active_player.target_hand
-      attack_hand = active_player.attack_hand
-      sticks = attack_hand.fingers
+      move = active_player.strategize(self)
+      if move.type == "split":
+        active_player.split()
+      elif move.type == "attack":
+        target_player = move.target_player
+        target_hand = move.target_hand
+        sticks = move.attack_hand.fingers
 
-      target_player.get_hit(sticks, target_hand)
+        target_player.get_hit(sticks, target_hand)
+      else:
+        raise ValueError("Invalid command given by player {}".format(active_player))
 
       self.next_player()
       self.print()
+
+class Move():
+  def __init__(self, type, attack_hand=0, target_player=0, target_hand=0):
+    self.type = type
+    self.attack_hand = attack_hand
+    self.target_player = target_player
+    self.target_hand = target_hand
+
+  def __str__(self):
+    if self.type == "attack":
+      return "{},{} attacks {},{}".format(self.attack_hand.owner.id, self.attack_hand.id, self.target_player.id, self.target_hand.id, self.target_hand.id)
+    elif self.type == "split":
+      return "{} splits".format(self.attack_hand.id)
+    else:
+      raise ValueError("Move constructed with invalid type {}".format(self.type))
